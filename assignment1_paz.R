7a94e76725603d1bdb24cc32c77be63f13c0b78d
paz64 <70188690+paz64@users.noreply.github.com>
2020-09-13 23:23:25 -0400

    Add files via upload

---
+++ assignment1_paz.R
@@ -0,0 +1,68 @@
+#________________________________________________________________
+#
+#                      PREAMBLE                    | start
+#________________________________________________________________
+
+# Clear workspace
+rm(list=ls())
+
+# Set working directory
+#setwd("D:/Fall 2020/EDU 291") #For Windows
+
+# Date:
+Sys.Date()
+
+# Attribution: Andrés Paz, used template given by Dr. Marsicano
+
+#________________________________________________________________
+#
+#                      TABLE OF CONTENTS           | table
+#________________________________________________________________
+#
+# Sections                                         | Keyword
+#________________________________________________________________
+#
+#   Preamble                                       | start
+#   Table of Contents                              | table
+#   Basic Calculation                              | war
+#   Objects, Vectors, and Lists                    | peace
+#   GitHub Repo                                    | git
+#________________________________________________________________
+#
+#                   BASIC CALCULATION              | war
+#________________________________________________________________
+
+# Basic Calculation
+
+(52 - 365) + 236
+sqrt(169)
+num <- 5^2
+37 * num
+
+#________________________________________________________________
+#
+#              OBJECTS, VECTORS, AND LISTS         | peace
+#________________________________________________________________
+
+# Objects
+working <- 37 * num
+grammys <- sqrt(169)
+working * grammys
+
+# Vectors
+lyrics <- c("what", "a", "way", "to", "make", "a", "livin")
+
+# Lists
+list("Working", working, lyrics)
+
+#________________________________________________________________
+#
+#                            GITHUB REPO            | git
+#________________________________________________________________
+
+# EDU291_paz
+
+#________________________________________________________________
+#
+#                                END
+#________________________________________________________________
